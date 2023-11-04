ee80e67623f2db5fac8954a93e985c7b917163f8
luacy200820 <luacy200820@gmail.com>
2023-11-04 15:34:58 +0800

    'bran1 add_commit script'

--- commit.sh
+++ commit.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-
-current=$(git branch | grep "*" | cut -b 3-)
-
 message=\'"$@"\'
+echo "$1"
 
-git add -A && git commit -a -m "$message"
-
-echo "wherer to push"
-read -i "$current" -e branch
-
-echo "You sure you wanna push?(Y/N)"
-read -i "y" -e yn
-
-if [ "$yn" == y ]; then
-	git push origin "$branch"
-else
-	echo "Have a nice day"
+if git rev-parse --verify $1; then
+	git checkout $1
+	git add -A
+	git commit -a -m "$message"
+	echo "add and commit finish"
+else 
+	echo "Not found this branch"
+	echo "Do you want to create new branch $1 ?(Y/N)"};
+	read -i "y" -e yn
+	if [ "$yn" == y ]; then
+		git checkout -b $1
+		echo "Finish..."
+		git commit -a -m "$message"
+		echo "add and commit finish"
+	else
+		echo "Bye"
+	fi
 fi
-
-
